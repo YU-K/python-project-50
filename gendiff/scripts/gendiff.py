c83760c2f46dba35931ffdf41014198afb73d3f9
YU-K <xver456@gmail.com>
2024-05-13 17:39:19 +0400

    Refactor gendiff module

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,24 +1,37 @@
 #!/usr/bin/env python
 import argparse
-import json
 import os
 import yaml
 from gendiff.generate_diff import generate_diff
+from gendiff.parsers import get_parser
+from gendiff.formatters.get_formatter import get_formatter
 
-parser = argparse.ArgumentParser(description='Compares two configuration '
-                                             'files and shows a difference.')
-parser.add_argument('first_file')
-parser.add_argument('second_file')
-parser.add_argument('-f', '--format', help='set format of output')
+arg_parser = argparse.ArgumentParser(
+    description='Compares two configuration '
+                'files and shows a difference.')
+arg_parser.add_argument('first_file')
+arg_parser.add_argument('second_file')
+arg_parser.add_argument('-f', '--format',
+                        default='stylish',
+                        help='set format of output (default: stylish)')
 
-args = parser.parse_args()
+args = arg_parser.parse_args()
+format_name = get_formatter(args.format)
 
+file_extension = os.path.splitext(args.first_file)[1][1:]
 
-def get_format():
-    file_extension = os.path.splitext(args.first_file)[1][1:]
-    if file_extension == 'yml':
-        return yaml
-    return json
+# TODO: создать функцию get_filepath  -
+#  получить путь до файла
+# TODO: создать функцию get_file_content -
+#  читает файл и возвращает данные из файла
+# TODO: создать функцию parse -
+#  парсит данные из файла в объекты, которые поддерживает ЯП
+
+# TODO: Добавьте текущий форматер как форматер по умолчанию для библиотеки.
+#       Это значит, что данный форматер применяется,
+#       если не указан какой-то другой
+# TODO: Укажите stylish как форматер по умолчанию в исполняемом файле
+# TODO: Добавьте в ридми аскинему с примером работы пакета c yaml
 
 
 first_file_path = f"{os.getcwd()}/{args.first_file}"
@@ -26,17 +39,19 @@ second_file_path = f"{os.getcwd()}/{args.second_file}"
 
 with open(first_file_path) as first_file_path1:
     with open(second_file_path) as second_file_path1:
-        format_file = get_format()
-        if format_file == yaml:
-            data1 = format_file.load(first_file_path1, Loader=yaml.Loader)
-            data2 = format_file.load(second_file_path1, Loader=yaml.Loader)
+        parser = get_parser(file_extension)
+        if parser == yaml:
+            data1 = parser.load(first_file_path1, Loader=yaml.Loader)
+            data2 = parser.load(second_file_path1, Loader=yaml.Loader)
         else:
-            data1 = format_file.load(first_file_path1)
-            data2 = format_file.load(second_file_path1)
+            data1 = parser.load(first_file_path1)
+            data2 = parser.load(second_file_path1)
 
 
 def main():
-    generate_diff(data1, data2)
+    # diff = generate_diff(data1, data2, plain)
+    diff = generate_diff(data1, data2, format_name)
+    print(diff)
 
 
 if __name__ == "main":
