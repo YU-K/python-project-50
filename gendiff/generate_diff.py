ceb12511ec422ead4bb9dfc71447b57505cbb841
YU-K <xver456@gmail.com>
2024-05-13 17:38:21 +0400

    Refactor generate_diff function

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,25 +1,7 @@
-def generate_diff(data1, data2):
-    result = ['{']
-    keys1 = list(data1.keys())
-    keys2 = list(data2.keys())
-    all_keys = sorted(set(keys1 + keys2))
+from gendiff.make_ast import make_ast
+from gendiff.formatters.stylish import stylish
 
-    for key in all_keys:
-        if key in data1 and key in data2:
-            if data1[key] != data2[key]:
-                result.append(f"  - {key}: {str(data1[key])}")
-                result.append(f"  + {key}: {str(data2[key])}")
-            else:
-                result.append(f"    {key}: {str(data1[key])}")
 
-        elif key in data1 and key not in data2:
-            result.append(f"  - {key}: {str(data1[key])}")
-        else:
-            result.append(f"  + {key}: {str(data2[key])}")
-
-    result.append('}')
-    for el in result:
-        print(el)
-    output = ''.join(result)
-
-    return output
+def generate_diff(filepath1, filepath2, format_name=stylish):
+    ast = make_ast(filepath1, filepath2)
+    return format_name(ast)
