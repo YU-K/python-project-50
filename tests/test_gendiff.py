ad82c0e8f610fbe31f9955e217cc00e749bf6459
YU-K <xver456@gmail.com>
2024-05-13 17:39:41 +0400

    Refactor test_gendiff module

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,25 +3,37 @@ import os
 import json
 import yaml
 from gendiff.generate_diff import generate_diff
+from gendiff.formatters.plain import plain
+import pytest
 
 
-first_file_path = f"{os.getcwd()}/tests/fixtures/file1.json"
-second_file_path = f"{os.getcwd()}/tests/fixtures/file2.json"
-result = f"{os.getcwd()}/tests/fixtures/output.txt"
-
-first_file_path_yml = f"{os.getcwd()}/tests/fixtures/file1.yml"
-second_file_path_yml = f"{os.getcwd()}/tests/fixtures/file2.yml"
+def get_fixture_path(filename: str) -> str:
+    current_dir = os.getcwd()
+    return os.path.join(current_dir, "tests/fixtures", filename)
 
 
-data1 = json.load(open(first_file_path))
-data2 = json.load(open(second_file_path))
-content_output = open(result).read()
+data1 = json.load(open(get_fixture_path("file1.json")))
+data2 = json.load(open(get_fixture_path("file2.json")))
+data1_r = json.load(open(get_fixture_path("file1_r.json")))
+data2_r = json.load(open(get_fixture_path("file2_r.json")))
+data1_yml = yaml.load(open(get_fixture_path("file1.yml")), Loader=yaml.Loader)
+data2_yml = yaml.load(open(get_fixture_path("file2.yml")), Loader=yaml.Loader)
+expected = open(get_fixture_path("output.txt")).read()
+expected_recur = open(get_fixture_path("output_r.txt")).read()
+expected_plain = open(get_fixture_path("output_plain.txt")).read()
 
-data1_yml = yaml.load(open(first_file_path_yml), Loader=yaml.Loader)
-data2_yml = yaml.load(open(second_file_path_yml), Loader=yaml.Loader)
-
-
+# TODO: добавить возможность подставлять различные файлы в pytest
 # @pytest.mark.parametrize("file1, file2, expected", [(first_file_path, second_file_path, result)])
-def test_generate_diff():
-    assert generate_diff(data1_yml, data2_yml) == content_output.strip()
-    # assert generate_diff(data1, data2) == content_output.strip()
+
+testdata = [
+            # (data1, data2, expected),
+            # (data1_yml, data2_yml, expected),
+            # (data1_r, data2_r, expected_recur),
+            (data1_r, data2_r, expected_plain),
+]
+
+
+@pytest.mark.parametrize("file1, file2, expected", testdata)
+def test_generate_diff(file1, file2, expected):
+    diff = generate_diff(file1, file2, plain)
+    assert diff == expected.strip()
