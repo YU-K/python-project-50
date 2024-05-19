c420717de7124218f046248587d3e8cd93cfc9ad
YU-K <xver456@gmail.com>
2024-05-19 19:02:23 +0400

    Refactor get_parser function

--- gendiff/parsers.py
+++ gendiff/parsers.py
@@ -3,7 +3,10 @@ import yaml
 
 
 def get_parser(file_extension):
-    if file_extension == 'json':
-        return json
-    elif file_extension in ['yml', 'yaml']:
-        return yaml
+    match file_extension:
+        case 'json':
+            return json.loads
+        case 'yml' | 'yaml':
+            return yaml.safe_load
+        case _:
+            raise Exception(f"Unknown format name {file_extension}!")
