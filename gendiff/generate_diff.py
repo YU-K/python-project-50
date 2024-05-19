2770ad68ed72c75c916e658a983550a3533e8cda
YU-K <xver456@gmail.com>
2024-05-19 19:00:52 +0400

    Refactor generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,7 +1,25 @@
+import os
 from gendiff.make_ast import make_ast
-from gendiff.formatters.stylish import stylish
+from gendiff.parsers import get_parser
+from gendiff.formatters.get_formatter import get_formatter
 
 
-def generate_diff(filepath1, filepath2, format_name=stylish):
-    ast = make_ast(filepath1, filepath2)
-    return format_name(ast)
+def read_file(filepath):
+    with open(filepath) as f:
+        content = f.read()
+    return content
+
+
+def generate_diff(file_path1, file_path2, format_name="stylish"):
+    file_content1 = read_file(file_path1)
+    file_content2 = read_file(file_path2)
+    file_extension = os.path.splitext(file_path1)[1][1:]
+    parse = get_parser(file_extension)
+
+    obj1 = parse(file_content1)
+    obj2 = parse(file_content2)
+
+    ast = make_ast(obj1, obj2)
+    formater = get_formatter(format_name)
+
+    return formater(ast)
