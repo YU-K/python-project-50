213d842df65b076e43e15397e8891ca118beb1ee
YU-K <xver456@gmail.com>
2024-05-19 19:01:48 +0400

    Add json formatter

--- gendiff/formatters/get_formatter.py
+++ gendiff/formatters/get_formatter.py
@@ -1,5 +1,6 @@
 from gendiff.formatters.plain import plain
 from gendiff.formatters.stylish import stylish
+from gendiff.formatters.json_format import json_format
 
 
 def get_formatter(format_name):
@@ -8,5 +9,7 @@ def get_formatter(format_name):
             return plain
         case 'stylish':
             return stylish
+        case 'json':
+            return json_format
         case _:
             raise Exception(f"Unknown format name {format_name}!")
